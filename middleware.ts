142fbf1c6a8abd00827092b994644b377fc3eff5
Nikolas Manuel <nikolas.manuel13@gmail.com>
2025-04-21 21:45:59 -0500

    Fixed middleware maybe

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,12 @@ import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/",
+  '/sign-in(.*)',
+  '/sign-up(.*)',
   '/:domain(.*)',
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhook/clerk",
-  "/api/webhook(.*)",
-  "/api(.*)",
-]);
+  '/api/webhooks(.*)',
+  '/api(.*)'
+])
 
 // Inlined at build time
 const ROOT_DOMAIN = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
@@ -44,7 +42,6 @@ export default clerkMiddleware(async (auth, req) => {
     );
   }
 
-  // 4️⃣ Fallback: protect other unknown routes on root
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
@@ -53,7 +50,13 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!api/webhook/clerk|_next/|[\\w-]+\\.\\w+).*)",
-    "/api/((?!webhook).*)",
+    /*
+     * Match all paths except for:
+     * 1. /api routes (except those we want to protect)
+     * 2. /_next (Next.js internals)
+     * 3. all static files (e.g. /favicon.ico)
+     */
+    "/((?!api/webhooks|_next/|[\\w-]+\\.\\w+).*)",
+    "/api/((?!webhooks).*)",
   ],
 };
