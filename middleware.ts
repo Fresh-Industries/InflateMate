fab7a00e9c2339910f5c372621f7afe87744014b
Nikolas Manuel <nikolas.manuel13@gmail.com>
2025-07-02 22:21:15 -0500

    fixed embeddings pt6

--- middleware.ts
+++ middleware.ts
@@ -86,13 +86,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
      * - embed (embed routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
      */
-    '/((?!api|embed|_next/static|_next/image|favicon.ico).*)',
+    '/((?!embed|_next/static|_next/image|favicon.ico).*)',
   ],
 };
