a1735bcf7cd1421d931b1f9bf992456b2a7e0b20
Nikolas Manuel <nikolas.manuel13@gmail.com>
2025-07-24 22:10:13 -0500

    fixed linter pt5

--- prisma/generated/prisma/edge.js
+++ prisma/generated/prisma/edge.js
@@ -508,7 +508,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\Nik\\InflateMate\\prisma\\generated\\prisma",
+      "value": "/Users/nikolasmanuel/inflatemate/prisma/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -517,12 +517,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\Nik\\InflateMate\\prisma\\schema.prisma",
+    "sourceFilePath": "/Users/nikolasmanuel/inflatemate/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -509,7 +509,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\Nik\\InflateMate\\prisma\\generated\\prisma",
+      "value": "/Users/nikolasmanuel/inflatemate/prisma/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -518,12 +518,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\Nik\\InflateMate\\prisma\\schema.prisma",
+    "sourceFilePath": "/Users/nikolasmanuel/inflatemate/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -586,8 +586,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "prisma/generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-darwin.dylib.node");
+path.join(process.cwd(), "prisma/generated/prisma/libquery_engine-darwin.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "prisma/generated/prisma/schema.prisma")
